d5d8ec40a0fe78a0494b7fe74d07fc0a9c94e11d
youyi <xzpwork@gmail.com>
2021-06-05 01:31:35 +0800

    优化动作+文本汉化

--- adb_shell.py
+++ adb_shell.py
@@ -7,6 +7,20 @@ import time
 import lxml.etree as ET
 
 
+def get_first_device():
+    command_list = ['adb', 'devices']
+    out = subprocess.Popen(command_list,
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.STDOUT)
+    out_str = out.communicate()[0].decode("utf-8").replace("\r", "")
+    keyword = "List of devices attached"
+    device_info = out_str[out_str.rindex(keyword) + len(keyword) + 1:]
+    device_list = device_info.split("\n")
+    if len(device_list) > 0 and len(device_list[0].split("\t")) == 2:
+        return device_list[0].split("\t")[0]
+    return None
+
+
 class AdbShell:
     def __init__(self, device_name=None):
         self.device_name = device_name
@@ -23,54 +37,56 @@ class AdbShell:
         point_start_y = random.randint(0, 100) + top + (bottom - top) * 3 / 5
         point_end_x = point_start_x
         point_end_y = random.randint(0, 100) + top + (bottom - top) / 5
-        print("start swipe node")
+        print("开始滑动页面去触发倒计时")
         self.run_adb_command("shell input swipe %d %d %d %d" % (point_start_x, point_start_y, point_end_x, point_end_y))
-        print("end swipe node")
 
     def dump_and_parse(self) -> ET.Element:
         work_dir = os.getcwd()
         dump_file_name = "baotao_ui_dump_%d.xml" % int(round(time.time() * 1000))
         android_path = "/sdcard/%s" % dump_file_name
         for i in range(0, 2):
-            if self.device_name is not None:
-                command_list = ['adb', '-s', self.device_name, 'shell', 'uiautomator', "dump", android_path]
-            else:
-                command_list = ['adb', 'shell', 'uiautomator', "dump", android_path]
+            command_list = ['adb', '-s', self.device_name, 'shell', 'uiautomator', "dump", android_path]
             out = subprocess.Popen(command_list,
                                    stdout=subprocess.PIPE,
                                    stderr=subprocess.STDOUT)
-            print("start dump ui")
-            stdout, error = out.communicate()
-            print("end dump ui, output:" + str(stdout))
-            if error is not None or "error" in str(stdout).lower():
-                print("dump error")
+            print("开始获取页面信息并保存")
+            try:
+                stdout, error = out.communicate(timeout=20)
+            except subprocess.TimeoutExpired:
+                print("获取页面信息超时,正在重试")
+                time.sleep(2)
                 continue
-            print("start pull dump file to local")
+            if out.returncode != 0 or error is not None or "error" in stdout.decode("utf-8").lower():
+                print(stdout.decode("utf-8"))
+                print("获取失败,正在重试")
+                time.sleep(2)
+                continue
+            print("正在将手机端页面信息拉取到本地")
             self.run_adb_command("pull %s %s" % (android_path, work_dir))
-            print("end pull dump file to local")
             xml_file_path = os.path.join(work_dir, dump_file_name)
             if not os.path.exists(xml_file_path):
-                print("dump file pull failed")
+                print("页面信息拉取失败，信息文件根本不存在")
                 continue
+            print("成功拉取到本地")
             et = ET.parse(xml_file_path).getroot()
-            print("start remove local and remote dump file")
+            print("解析成功，开始移除本地和手机端多余的信息文件")
             self.run_adb_command("shell rm %s" % android_path)
             os.remove(xml_file_path)
-            print("end remove local and remote dump file")
+            print("完成移除")
             return et
-        print("the page ui animation too busy, uiautomator can't dump")
+        print("页面动画过于频繁导致工具无法抓取信息:(")
         return None
 
     def back(self):
-        print("start click back")
+        print("返回上一个页面")
         self.run_adb_command("shell input keyevent 4")
         time.sleep(3)
-        print("end click back")
+        print("完成返回")
 
     def wake(self):
-        print("start wake phone")
+        print("唤醒手机屏幕防止息屏")
         self.run_adb_command("shell input keyevent 224")
-        print("end wake phone")
+        print("完成唤醒")
 
     def click_node(self, node):
         bounds = str(node.get("bounds"))
@@ -82,16 +98,12 @@ class AdbShell:
         bottom = int(match_result.group(4))
         pointx = random.randint(left + 1, right - 1)
         pointy = random.randint(top + 1, bottom - 1)
-        print("start click node")
+        print("开始点击对应的按钮")
         self.run_adb_command("shell input tap %d %d" % (pointx, pointy))
         time.sleep(4)
-        print("end click node")
+        print("完成点击")
 
     def run_adb_command(self, command):
-        if self.device_name is not None:
-            full_command = "adb -s %s %s" % (self.device_name, command)
-        else:
-            full_command = "adb %s" % command
-        print("start run adb command: %s" % full_command)
+        full_command = "adb -s %s %s" % (self.device_name, command)
+        print("运行相关命令: %s" % full_command)
         os.system(full_command)
-        print("end run adb command")
